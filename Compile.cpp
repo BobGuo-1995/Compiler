06310e23f38a280b6be3a79be5145223c95222aa
Bob(Jue), Guo <50707331+BobGuo-1995@users.noreply.github.com>
2019-06-02 13:16:25 -0400

    Update Compile.cpp

--- Compile.cpp
+++ Compile.cpp
@@ -8,7 +8,6 @@ int main(int nargs, char *args[])
 	
 	string fname="test.txt";
 	string fnameOutC;
-	//string fname(R"(E:\temp\DavidLane\info for lexical analyer 2\info for lexical analyer\error test.txt)");
 	if (nargs == 2)fname = args[1];
 	fnameOutC = fname + ".c";
 	TokenStream sToken;
